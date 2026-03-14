c14a3f7d3105759da7565d1befadb00212f1c94f
aryanpingle <realaryanpingle@gmail.com>
2026-03-14 16:42:38 +0530

    Article: Website

--- app/components/ArticleRenderer.vue
+++ app/components/ArticleRenderer.vue
@@ -5,7 +5,7 @@ import ISLAND_IMAGE from "~/assets/frontpage/island.png";
 import XKCD_IMAGE from "~/assets/frontpage/xkcd.png";
 
 const HERO_TEXT = `
-Gongs of the Gion Shoja bells echo the impermanence of all things. The color of the sala flowers reveals the truth that the prosperous must decline. But he is the exception - in this exclusive issue, we explore the psyche of 24 year old Aryan Pingle.
+Gongs of the _Gion Shoja_ bells echo the impermanence of all things. The color of the sala flowers reveals the truth that the prosperous must decline. But he is the exception — in this exclusive issue of The Daily Pingle, we explore the psyche of our editor-in-chief, 24 year old Aryan Pingle.
 
 While others coward behind platitudes of fake humility, he boldly proclaims himself to be a 'Web Developer Extraordinaire'. But don't be fooled by the sheer magnificence of his aptitude in making stunning websites, for his peers deem him a master of adaptability; he makes container management scripts as skillfully as he prototypes in Figma. A "Jack of all trades" in the truest sense. This charismatic prodigy is the single greatest asset to the fields of Compiler Construction, Meta-programming, and Engineering in general. With the sole exception of Terry Davis, of course.`;
 
@@ -14,8 +14,13 @@ After a legendary 1 year run at _Zomato_, senior executives at _Eternal_ have de
 `;
 
 const CONTACT_TEXT = `
-But you can get in touch with your daddy via Instagram: [@aryan.pingle](https://www.instagram.com/aryan.pingle/). If you're a nerd, you can find me on GitHub: [github/aryanpingle](https://github.com/aryanpingle/). If you're a dinosaur, you can e-mail me at [realaryanpingle@gmail.com](mailto://realaryanpingle@gmail.com). If you're a recruiter, I assure you that I don't mean any of the ridiculous stuff I've written. And you may connect with me on LinkedIn: [linkedin/aryan-pingle](https://www.linkedin.com/in/aryan-pingle/).
+But you can get in touch with the newspaper representatives on Instagram by sending us cool reels: [@aryan.pingle](https://www.instagram.com/aryan.pingle/). Nerds can reach out to us on GitHub: [github/aryanpingle](https://github.com/aryanpingle/). If you're a dinosaur, you can e-mail us at [realaryanpingle@gmail.com](mailto://realaryanpingle@gmail.com). If you're a recruiter, we assure you that we here at _The Daily Pingle_ don't mean any of the ridiculous stuff we've written. And you may connect with us on LinkedIn: [linkedin/aryan-pingle](https://www.linkedin.com/in/aryan-pingle/).
 `;
+
+const WEBSITE_TEXT = `
+The digital experience of this prestigious newspaper was made with [Nuxt](https://nuxt.com/), which uses the [VueJS](https://vuejs.org/) framework. I had a blast making it, which was largely due to how fun it is to use this framework. I'll keep trying to optimize the shit outta this site, which might mean ditching frameworks entirely. But I won't forget you, Vue.
+
+Absolutely no AI was used for any part of this newspaper — it simply cannot measure up to my extremely high standards.`;
 
 defineProps<{ articleId: ArticleId }>();
 </script>
@@ -52,6 +57,13 @@ defineProps<{ articleId: ArticleId }>();
     v-if="$props.articleId === 'contact'"
     title="You're out of touch"
     :text="CONTACT_TEXT"
+  ></Article>
+
+  <Article
+    v-if="$props.articleId === 'website'"
+    title="Vue from the top"
+    byline="Aryan Pingle / Shibuya / March 14, 2001"
+    :text="WEBSITE_TEXT"
   ></Article>
 </template>
 
--- app/utils/frontpage.ts
+++ app/utils/frontpage.ts
@@ -4,7 +4,9 @@ export type ArticleId =
   | "eternal"
   | "island"
   | "xkcd"
-  | "contact";
+  | "contact"
+  | "website"
+  | "education";
 
 export type ColumnwiseArticles = {
   1?: ArticleId[];
@@ -21,17 +23,17 @@ export type Breakpoints = {
 export const ARTICLES: Breakpoints = {
   // Mobile
   sm: {
-    "1": ["hero", "island", "contact", "eternal", "drinker", "xkcd"],
+    "1": ["hero", "island", "contact", "eternal", "drinker", "website", "xkcd"],
   },
   // Tablet
   md: {
-    "1": ["island", "contact", "drinker", "xkcd"],
-    "2": ["hero", "eternal"],
+    "1": ["island", "contact", "drinker", "xkcd", "education"],
+    "2": ["hero", "eternal", "website"],
   },
   // Desktop
   lg: {
     "1": ["island", "drinker"],
-    "2": ["hero"],
+    "2": ["hero", "website"],
     "3": ["contact", "eternal", "xkcd"],
   },
 };
