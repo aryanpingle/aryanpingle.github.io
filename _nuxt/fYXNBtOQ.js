4719588226e75857a9e375bde069dee2bdaf0074
aryanpingle <aryanpingle@users.noreply.github.com>
2026-07-12 12:00:22 +0000

    deploy: 53f81d49ecb940183e4fe944d65dab4caf4ba2aa

---
+++ _nuxt/fYXNBtOQ.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,I as o,o as r}from"./L2_Anxcj.js";const t={};function n(e,c){return r(),a("table",null,[o(e.$slots,"default")])}const _=Object.assign(s(t,[["render",n]]),{__name:"ProseTable"});export{_ as default};
